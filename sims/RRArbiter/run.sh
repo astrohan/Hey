612e903b1a914161f36b02f07f2e1c83e67ed12b
astrohan <32398654+astrohan@users.noreply.github.com>
2024-03-23 15:58:01 +0900

    Delete sims/RRArbiter directory

--- sims/RRArbiter/run.sh
+++
@@ -1,3 +0,0 @@
-rm -rf workdir
-verilator --main --timing --cc --exe --build -O2 -sv +1800-2017ext+sv --assert --timescale 1ns/1ps --trace-fst --trace-threads 2 --Mdir workdir --top-module top -I${project}/rtl_codes/commons CommonUtils.sv top.sv RRArbiter.sv
-./workdir/Vtop
