e1efcbcee89a641c2fd602675d84495e41bdf696
astrohan <astrohan84@gmail.com>
2024-04-09 02:15:40 +0900

    update some common codes

--- sims/commons/async_fifo_tb.sv
+++ sims/commons/async_fifo_tb.sv
@@ -1,22 +1,53 @@
-module rrarbiter_tb;
+module async_fifo_tb;
 
-  parameter int nReq  = 16   ;
-  bit         clock   ;
-  bit         reset   = 1;
-  bit         trigger   ;
-  bit   [nReq-1:0]  request   ;
-  bit   [nReq-1:0]  grant   ;
+  parameter int FifoDepth = 2 ;
+  parameter int DataWidth = 4 ;
 
-  rrarbiter #(nReq) dut(.*);
+  bit                   wclk    ;
+  bit                   wreset  = 1;
+  bit                   rclk    ;
+  bit                   rreset  = 1;
+  bit                   wvalid  ;
+  bit [DataWidth-1:0]   wdata   ;
+  bit                   wready  ;
+  bit                   rvalid  ;
+  bit [DataWidth-1:0]   rdata   ;
+  bit                   rready  = 1;
 
-  initial forever #1ns clock = ~clock;
+  async_fifo #(
+    .FifoDepth(FifoDepth),
+    .DataWidth(DataWidth)
+  ) dut(.*);
+
+  always_ff @(posedge wclk) begin
+    if(wvalid)
+      wdata <= wdata + 1;
+  end
+
+  int wfreq = 100; // [MHz]
+  int rfreq = 100; // [MHz]
+  real whalf = 1000.0/wfreq;
+  real rhalf = 1000.0/rfreq;
+  initial #0.5 forever #whalf wclk = ~wclk;
+  initial #0.7 forever #rhalf rclk = ~rclk;
+
   initial begin
-    #100ns
-    reset = 0;
     #10ns
-    trigger = 1;
-    request = '1;
-    #100ns
+    fork
+      begin
+        @(posedge wclk)
+        #0.1
+        wreset = 0;
+      end
+      begin
+        @(posedge rclk)
+        #0.1
+        rreset = 0;
+      end
+    join
+    #20ns
+    wvalid  = 1;
+    #10us
     $finish(2);
   end
 
@@ -31,4 +62,4 @@ module rrarbiter_tb;
     $fwrite(32'h80000002, "simulation finished due to time-out\n");
     $finish(2);
   end
-endmodule: rrarbiter_tb
+endmodule: async_fifo_tb
